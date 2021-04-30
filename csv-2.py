0736ef82f831b96746778c026088c60fd0c86eba
Lauren <lm3623@nyu.edu>
2021-04-30 21:45:52 +0400

    finalized csv.py, final commit

--- csv-2.py
+++ csv-2.py
@@ -5,6 +5,7 @@ import matplotlib.pyplot as plt
 x = []
 y1 = []
 y2 = []
+c=0
 
 with open("CWND.csv","r") as fp:
 	fp.readline()
@@ -13,15 +14,18 @@ with open("CWND.csv","r") as fp:
 		y1.append(float(data[0]))
 		x.append(float(data[1]))
 		y2.append(float(data[2]))
+		c = c + 1
 
 fig, ax1 = plt.subplots()
 ax2 = ax1.twinx()
 
+plt.xlim(x[0],x[c-1])
+
 ax1.plot(x,y1,"g--")
-ax1.set_ylim([0,70])
+ax1.set_ylim([0,15])
 
 ax2.step(x,y2,"r--")
-ax2.set_ylim([0,70])
+ax2.set_ylim([0,15])
 
 ax1.set_ylabel("Window size")
 ax1.set_xlabel("Time (Sec)")
